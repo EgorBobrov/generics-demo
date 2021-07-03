628d7841d04fb2bf53b8228f43cf773baa5499eb
Egor_Bobrov <egorbobrov1776@gmail.com>
2021-07-03 18:38:18 +0300

    Changed class name to better reflect what's inside

--- src/com/egobob/generics/examples/UpperAndLowerBoundWildcardExample.java
+++ src/com/egobob/generics/examples/UpperAndLowerBoundWildcardExample.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class UpperBoundWildcardExample {
+public class UpperAndLowerBoundWildcardExample {
 
     public static List<Person> allContacts = new ArrayList<>();
 
