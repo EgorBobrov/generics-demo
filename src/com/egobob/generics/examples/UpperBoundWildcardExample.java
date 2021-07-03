53c48db5310e2ba82a42b00919c6a735bade803d
Egor_Bobrov <egorbobrov1776@gmail.com>
2021-07-03 18:35:19 +0300

    Initial commit

---
+++ src/com/egobob/generics/examples/UpperBoundWildcardExample.java
@@ -0,0 +1,43 @@
+package com.egobob.generics.examples;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class UpperBoundWildcardExample {
+
+    public static List<Person> allContacts = new ArrayList<>();
+
+    public static List<Customer> customers = new ArrayList<>();
+
+    public static abstract class Person {
+        public String emailAddress;
+        public String name;
+    }
+
+    public static class Customer extends Person {
+        public boolean vipStatus;
+        public Map<String, Integer> orders;
+    }
+
+    public static class Supplier extends Person {
+        public Map<String, Double> productPrices;
+    }
+
+    public static void sendEmails(List<? extends Person> list, String update) {
+        for (Person person : list) {
+            // imagine that email sending logic is here
+            System.out.printf("Sent company update [%s] to: %s using email: %s",
+                    update, person.name, person.emailAddress);
+        }
+    }
+
+    public static void addCustomerToList(Customer customer, List<? super Customer> list) {
+        list.add(customer);
+        System.out.printf("Added customer: %s to the list", customer.name);
+    }
+
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/com/egobob/generics/examples/WildcardExample.java
@@ -0,0 +1,46 @@
+package com.egobob.generics.examples;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class WildcardExample {
+
+    public static void printAnyObjectList(List<?> list) {
+        for (Object o: list) {
+            System.out.println(o.toString());
+        }
+    }
+
+    public static Number maxNumber(List<? extends  Number> numbers) {
+        return numbers.stream()
+                .max(Comparator.comparingDouble(Number::doubleValue))
+                .orElseThrow();
+    }
+
+    public static void addAnObjectToAnyObjectList(List<Object> list) {
+        list.add(new Object());
+    }
+
+    public static void demonstrateTypeErasure() {
+        List<String> strings = new ArrayList<>();
+        String stringValue = "String value";
+        strings.add(stringValue);
+        String string = strings.get(0);
+
+        List probablyStrings = new ArrayList();
+        String definitelyString = "String value";
+        if (definitelyString instanceof String) {
+            probablyStrings.add(definitelyString);
+        } else {
+            throw new RuntimeException("What are you doing? This List is supposed to contain strings!");
+        }
+        String probablyString = (String) probablyStrings.get(0);
+
+    }
+
+    public static void main(String[] args) {
+        List<String> strings = List.of("first", "second");
+        printAnyObjectList(strings);
+    }
+}
